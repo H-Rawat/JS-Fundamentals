81d88f090b496eaca65ddc7f6a6df72f4312abe5
Himanshu Rawat <hrawat9958@gmail.com>
2022-05-15 13:15:39 +0530

    data types

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
 # JS-Fundamentals
-
-<!-- Variables -->
-
-let or const
-
-- cant assign a const any value again
-- can reassign values to a "let" variable
-
-reassigning =>
-let tempVar = 1;
-tempVar = 2;
-
-- dont use var
--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+// VARIABLES
+// let or const
+
+// - cant assign a const any value again
+// - can reassign values to a "let" variable
+
+// reassigning =>
+// let tempVar = 1;
+// tempVar = 2;
+
+// - dont use var
+
 // const myVar = 250;
 // console.log(myVar);
 
 // let temp = 11;
 // console.log(temp);
+
+// DATA TYPES
+// string
+// const myString = "hey";
+
+// boolean
+// const temp = true;
+
+// arrays
+// const friends = [22, true, "friend"];
+// console.log(friends);
+// console.log(friends[1]);
+// console.log(friends[3]); => returns undefined
+
+// undefined is a value of a variable which hasnt been initialized
+
+// big int
+// const bigIntNum =
+//   111111111111111111111111111111111111111111111111111111111111111111111111n;
+// console.log(bigIntNum);
+
+// null
+// let tempVar = null;
+// this says that this variable's value doesnt exist
